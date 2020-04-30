ac50bd171a9133fac6a88b59b85ad3728e8b2cbf
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-04-30 11:38:45 +0100

    Added ncdc scrapper

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const axios = require('axios')
+const cheerio = require('cheerio');
 const router = express.Router();
 
 const Country = require('../model/countries');
+
+const url = 'https://covid19.ncdc.gov.ng/';
+
+const trimText = text => {
+  return JSON.parse(JSON.stringify(text).replace(/\\n/g, ''));
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +21,28 @@ router.get('/', function(req, res, next) {
     }
   })
 });
+
+router.get('/nigeria', (req, response) => {
+  axios(url).then(res => {
+    const html = res.data;
+    const $ = cheerio.load(html);
+    const table = $('#custom3 > tbody > tr');
+    const total = [];
+    table.each((index, tr) => {
+      if (index !== 0) {
+        const stateName = $(tr).find('td:nth-child(1)').text();
+        const cases = $(tr).find('td:nth-child(3)').text();
+        total.push({
+          name: trimText(stateName),
+          cases: parseInt(trimText(cases))
+        });
+      }
+    })
+    response.status(200).json(total);
+  }).catch(err => {
+    response.status(500).json({ message: 'Data not found' });
+  })
+})
 
 router.get('/:country', (req, res) => {
   Country.find({name: new RegExp(`${req.params.country}$`, 'i')}, (err, data) => {
